f71251f6bd4bdee144ca0c9e4253cad67703d487
v1xohay_ <98161055+v1xohay@users.noreply.github.com>
2023-08-06 16:33:40 +0300

    New update

--- Sppr4t.py
+++ Sppr4t.py
@@ -3,94 +3,167 @@ import os
 import sys
 import time
 import socket
-from time import sleep
+import datetime
+import os
+import math
+import threading
+import sqlite3
 
-loading = int(1)
 
-for loading in range(loading):
-    rand2 = ["0.1", "0.3", "0.6", "0.2"]
-    response = os.system("clear")
-    for i in range(101):
-        j = float(random.choice(rand2))
-        sys.stdout.write('\r')
-        a = float(random.choice(rand2))
-        if i > 10:
-            a = 0
-        if i > 29:
-            a = j
-        if i > 33:
-            a = 0
-        if i > 60:
-            a = j
-        if i > 68:
-            a = 0
-        time.sleep(a)
-        sys.stdout.write("loading sppr console [%-10s] %d%%" % ('='*i, 1*i))
-        sys.stdout.flush()
-        sleep(0.08)
+scan_results = {
+    "getip": [],
+    "portscan": [],
+}
+
+def clear_screen():
+    if os.name == 'nt':
+        os.system("cls")
+    else:
+        os.system("clear")
+
+rand2=['0.1','0.3','0.6','0.2']
+for loading in range(0):
+	j=float(random.choice(rand2));sys.stdout.write('\r');a=float(random.choice(rand2))
+	if loading>10:a=0
+	if loading>29:a=j
+	if loading>33:a=0
+	if loading>60:a=j
+	if loading>68:a=0
+	time.sleep(a);sys.stdout.write('loading SppR console [%-10s] %d%%'%('='*loading,loading));sys.stdout.flush();time.sleep(.08)
+
+report_counter = 1
+def save_scan_result(scan_type, result):
+    conn = sqlite3.connect('scan_results.db')
+    cursor = conn.cursor()
+
+    # Создание таблицы, если она не существует
+    cursor.execute('CREATE TABLE IF NOT EXISTS scan_results (id INTEGER PRIMARY KEY AUTOINCREMENT, scan_type TEXT, result TEXT, timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP)')
+
+    # Вставка данных сканирования
+    cursor.execute('INSERT INTO scan_results (scan_type, result) VALUES (?, ?)', (scan_type, result))
+
+    conn.commit()
+    conn.close()
+
+def generate_report():
+    global report_counter
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    report_filename = f"report_{timestamp}_{report_counter}.txt"
+
+    with open(report_filename, "w") as report_file:
+        report_file.write("**** Scan Report ****\n")
+
+        report_file.write("\n--- getip Scan Results ---\n")
+        if scan_results["getip"]:
+            for index, result in enumerate(scan_results["getip"], 1):
+                report_file.write(f"Domain: {result['domain']}\nIP: {result['ip']}\nScan Time: {result['scan_time']:.1f} seconds\n")
+                report_file.write(f"Timestamp: {result['timestamp']}\n")
+                if index < len(scan_results["getip"]):
+                    report_file.write("-------------------\n")
+        else:
+            report_file.write("No getip scans have been performed.\n\n")
+
+        report_file.write("\n--- Port Scan Results ---\n")
+        if scan_results["portscan"]:
+            for index, result in enumerate(scan_results["portscan"], 1):
+                report_file.write(f"Target IP: {result['target_ip']}\n")
+                if result['open_ports']:
+                    report_file.write(f"Open Ports: {', '.join(map(str, result['open_ports']))}\n")
+                else:
+                    report_file.write("No open ports found.\n")
+                report_file.write(f"Scan Time: {result['scan_time']:.1f} seconds\n")
+                report_file.write(f"Timestamp: {result['timestamp']}\n")
+                if index < len(scan_results["portscan"]):
+                    report_file.write("-------------------\n")
+        else:
+            report_file.write("No port scans have been performed.\n\n")
+
+    print(f"Scan report has been saved to {report_filename}")
+    report_counter += 1
 
 
 def logo():
-    respone = os.system("clear")
-    print("  .                                                                       .      ")
-    print(" 4                .                                       .                t     ")
-    print("dX.             .dXb    __                         __    dXb.             .Xb    ")
-    print("9XXb._       _.dXXXXb dXXXXbo.                 .odXXXXb dXXXXb._       _.dXXP    ")
-    print(" 9XXXXXXXXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXXXXXP     ")
-    print("  `9XXXXXXXXXXXXXXXXXXXXX'~   ~`OOO8b   d8OOO'~   ~`XXXXXXXXXXXXXXXXXXXXXP'      ")
-    print("    `9XXXXXXXXXXXP' `9XX'          `98v8P'          `XXP' `9XXXXXXXXXXXP'        ")
-    print("        ~~~~~~~       9X.          .db|db.          .XP       ~~~~~~~            ")
-    print("                        )b.  .dbo.dP'`v'`9b.odb.  .dX(                           ")
-    print("                      ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.                          ")
-    print("                     dXXXXXXXXXXXP'   .   `9XXXXXXXXXXXb                         ")
-    print("                   dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb                         ")
-    print("                   9XXb'   `XXXXXb.dX|Xb.dXXXXX'   `dXXP                         ")
-    print("                     `'      9XXXXXX(   )XXXXXXP      `'                         ")
-    print("                              XXXX X.`v'.X XXXX                                  ")
-    print("                              XP^X'`b   d'`X^XX                                  ")
-    print("                              X. 9  `   '  P )X                                  ")
-    print("                              `b  `       '  d'                                  ")
-    print("                               `    SppR4t   '                                   ")
-    print("           | donations: https://www.donationalerts.com/r/v1xohay_ |              ")
-    print("                          | type help for commands |                             ")
-    print("                          |   created by v1xohay_  |                             ")
+    clear_screen()
+    with open('ASCII.txt', 'r') as file:
+        ascii_art = file.read()
+    print(ascii_art)
 
     print("\n")
     mainfunc()
 
+def scan_ports(target_ip):
+    open_ports = []
+    ports_to_scan = [20, 21, 22, 23, 25, 53, 80, 443, 8080, 110, 143, 587, 3306, 3389, 5900]
+
+    for port in ports_to_scan:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((target_ip, port))
+        if result == 0:
+            open_ports.append(port)
+        sock.close()
+
+    return open_ports
+
+def port_scan_worker(target_ip, start_time):
+    try:
+        open_ports = scan_ports(target_ip)
+        end_time = datetime.datetime.now()
+        elapsed_time = end_time - start_time
+
+        result_text = f"Scan complete in {elapsed_time.total_seconds():.2f} seconds, result:"
+        result_text += f"\nTarget IP: {target_ip}"
+        if open_ports:
+            result_text += f"\nOpen Ports: {', '.join(map(str, open_ports))}"
+        else:
+            result_text += "\nNo open ports found."
+
+        print(result_text)
+
+        scan_results["portscan"].append({
+            "target_ip": target_ip,
+            "open_ports": open_ports,
+            "scan_time": elapsed_time.total_seconds(),
+            "timestamp": end_time.strftime('%Y-%m-%d %H:%M:%S'),
+        })
+        
+        save_scan_result('portscan', result_text)
+
+    except socket.gaierror:
+        print("Error: Could not resolve the target IP address. Please check if the IP is valid.")
+    except Exception as e:
+        print("An error occurred:", str(e))
+
 def mainfunc():
-    for rand in range(1):
-        rand = ["1", "2", "3", "4", "5", "6", "7", "8",
-                "9", "10", "11", "12", "13", "14", "15", "16"]
-        alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-        secs = ['1.2', '0.7', '1', '0.4', '1.5']
-        r_secs = float(random.choice(secs))
-        pass1 = (random.choice(alph))
-        pass2 = int(random.choice(rand))
+    alph = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    secs = ['1.2', '0.7', '1', '0.4', '1.5']
+    r_secs = float(random.choice(secs))
+    pass1 = (random.choice(alph))
+    pass2 = int(random.choice(range(1, 17)))
+
+    command_help = {
+        "getip": "Get the IP address of a domain (example.com).",
+        "banner": "Clear the console screen and display the program's banner.",
+        "clear": "Clear the console screen.",
+        "time": "Show the current time and year.",
+        "ver": "Display the current version of the program.",
+        "help": "Show a list of available commands.",
+        "exit": "Exit the program.",
+        "report": "Generate and save a scan report.",
+        "portscan": "Scan open ports on a target IP address.",
+    }
+
     main = input("SppR>")
 
     if main == "help":
-        print("ver")
-        print("time")
-        print("banner")
-        print("exit")
-        print("getip")
+        print("Available commands:")
+        for command, description in command_help.items():
+            print(f"{command}: {description}")
         mainfunc()
 
     if main == "ver":
-        print("you currently using SppR4t pre-alpha version!")
-        mainfunc()
-
-    if main == "getip":
-        domain = input("domain(example.com):")
-        output_ip = socket.gethostbyname(domain)
-        print("starting getip on " + domain + "...")
-        time.sleep(r_secs)
-        print("scan complete in", end=" ")
-        print(r_secs, end=" ")
-        print(" seconds, result:")
-        print("domain: " + domain + "\n" + "ip: " + output_ip)
+        print("You are currently using SppR4t pre-alpha version!")
         mainfunc()
 
     if main == "time":
@@ -101,16 +174,68 @@ def mainfunc():
         print("year:", cur_year)
         mainfunc()
 
+    if main == "report":
+        generate_report()
+        mainfunc()
+
     if main == "banner":
         logo()
+    
+    if main == "clear":
+        clear_screen()
 
     if main == "exit":
         exit()
 
-    if main == pass1*pass2:
+    if main == pass1 * pass2:
         exit()
+        
+    if main == "getip":
+        domain = input("Enter the domain (example.com): ")
+        try:
+            output_ip = socket.gethostbyname(domain)
+            print("Starting getip on " + domain + "...")
+            start_time = datetime.datetime.now()
+            end_time = datetime.datetime.now()
+            elapsed_time = end_time - start_time
+            print(f"Scan complete in {elapsed_time.total_seconds():.2f} seconds, result:")
+            print(f"Domain: {domain}\nIP: {output_ip}\nTime: {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
+
+            scan_results["getip"].append({
+                "domain": domain,
+                "ip": output_ip,
+                "scan_time": round(elapsed_time.total_seconds(), 2),
+                "timestamp": end_time.strftime('%Y-%m-%d %H:%M:%S'),
+            })
+
+            save_scan_result('getip', f"Domain: {domain}\nIP: {output_ip}\nTime: {end_time.strftime('%Y-%m-%d %H:%M:%S')}")
+
+        except socket.gaierror:
+            print("Error: Could not resolve the domain name. Please check if the domain is valid.")
+            mainfunc()
+        except Exception as e:
+            print("An error occurred:", str(e))
+            mainfunc()
+        mainfunc()
+
+    if main == "portscan":
+        target_ip = input("Enter the target IP address: ")
+        try:
+            print("Starting portscan on " + target_ip + "...")
+            start_time = datetime.datetime.now()
+
+            port_scan_thread = threading.Thread(target=port_scan_worker, args=(target_ip, start_time))
+            port_scan_thread.start()
+
+            port_scan_thread.join()
+
+        except Exception as e:
+            print("An error occurred:", str(e))
+
     else:
-        logo()
+        print("\033[1;31mInvalid command. Type 'help' for a list of commands.\033[0m")
+        mainfunc()
+    mainfunc()
 
-
-logo()
+if __name__ == "__main__":
+    logo()
